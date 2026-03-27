8a2664bfb149f89c2af1117da2bffcb51d70565f
Ismael-devTR <dev.tristan.romero@gmail.com>
2026-03-27 10:34:30 -0600

    fix redirections

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const basePath = "/farm";
 const nextConfig: NextConfig = {
   output: "standalone",
   basePath,
+  skipTrailingSlashRedirect: true,
   env: {
     NEXT_PUBLIC_BASE_PATH: basePath,
   },
